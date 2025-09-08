bd2b4a21ee10b83bccbb1d2bfe04fa0d60eab6f1
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-08 01:55:39 -0300

    Ajustes Infra

--- iac/infra/modules/aks/network.tf
+++ iac/infra/modules/aks/network.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "aks_vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "aks-vnet"
   location            = var.location
   resource_group_name = var.resource_group
@@ -8,14 +8,49 @@ resource "azurerm_virtual_network" "aks_vnet" {
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = var.resource_group
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "aks-delegation"
+    service_delegation {
+      name = "Microsoft.ContainerService/managedClusters"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
 
-resource "azurerm_subnet" "appgw_subnet" {
-  name                 = "appgw-subnet"
-  resource_group_name  = var.resource_group
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.aks_nsg.id
 }
 
