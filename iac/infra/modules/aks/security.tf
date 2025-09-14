c128624ce2257b0ff60f03ab5cf50585eeb70f1c
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-14 02:11:23 -0300

    Ajustes Helms

--- iac/infra/modules/aks/security.tf
+++ iac/infra/modules/aks/security.tf
@@ -15,7 +15,7 @@ resource "azurerm_role_assignment" "ip_permission" {
 resource "azurerm_role_assignment" "aks_network_contributor" {
   scope                = azurerm_subnet.aks_subnet.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   depends_on           = [azurerm_kubernetes_cluster.aks]
 }
 
