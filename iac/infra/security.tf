5bbc421e2a581152d94658465f022500f9afab45
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-19 00:11:31 -0300

    Ajustes Nginx

--- iac/infra/security.tf
+++ iac/infra/security.tf
@@ -7,18 +7,3 @@ resource "azurerm_role_assignment" "identity_storage_access" {
   ]
 }
 
-resource "azurerm_role_assignment" "purview_data_curator" {
-  scope                = azurerm_purview_account.catalogo.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azurerm_user_assigned_identity.integration_identity.principal_id
-  depends_on = [
-    azurerm_purview_account.catalogo
-  ]
-}
-
-resource "null_resource" "register_purview" {
-  provisioner "local-exec" {
-    command = "az provider register --namespace Microsoft.Purview"
-  }
-}
-
