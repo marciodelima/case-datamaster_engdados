97ecfba04797320fff7527f6574b74084cd74c32
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-20 00:20:42 -0300

    Ajustes Security

--- iac/infra/modules/aks/security.tf
+++ iac/infra/modules/aks/security.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "aks_to_acr" {
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].userAssignedIdentities.principal_id
   role_definition_name = "AcrPull"
   scope                = data.azurerm_container_registry.acr.id
   depends_on           = [azurerm_kubernetes_cluster.aks]
@@ -8,14 +8,14 @@ resource "azurerm_role_assignment" "aks_to_acr" {
 resource "azurerm_role_assignment" "ip_permission" {
   scope                = azurerm_public_ip.nginx_ingress_ip.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].userAssignedIdentities.principal_id
   depends_on           = [azurerm_kubernetes_cluster.aks]
 }
 
 resource "azurerm_role_assignment" "aks_network_contributor" {
   scope                = azurerm_subnet.aks_subnet.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].userAssignedIdentities.principal_id
   depends_on           = [azurerm_kubernetes_cluster.aks]
 }
 
@@ -31,12 +31,14 @@ data "azurerm_resource_group" "aks_rg" {
 resource "azurerm_role_assignment" "aks_network_contributor_rsg" {
   scope                = data.azurerm_resource_group.node_rg.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].userAssignedIdentities.principal_id
+  depends_on           = [azurerm_kubernetes_cluster.aks]
 }
 
 resource "azurerm_role_assignment" "aks_network_contributor_rsg2" {
   scope                = data.azurerm_resource_group.aks_rg.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].userAssignedIdentities.principal_id
+  depends_on           = [azurerm_kubernetes_cluster.aks]
 }
 
