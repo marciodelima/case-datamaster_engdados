1b0d2aaec01140aae8929bd0b840c714cf0f5fc8
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-27 19:45:31 -0300

    APPs

--- iac/infra/modules/alerts/alertas.tf
+++ iac/infra/modules/alerts/alertas.tf
@@ -18,8 +18,10 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "job_failure_alert" {
   enabled             = true
   frequency           = 5
   time_window         = 5
-  scopes              = [azurerm_log_analytics_workspace.logs.id]
-  query               = <<QUERY
+
+  data_source_id = azurerm_log_analytics_workspace.logs.id
+
+  query = <<QUERY
 AzureDiagnostics
 | where Category == "WorkspaceLogs"
 | where OperationName == "JobRun"
@@ -42,17 +44,21 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "job_duration_alert" {
   name                = "job-duration-alert"
   resource_group_name = var.resource_group_name
   location            = var.location
-  scopes              = [var.workspace_logs_id]
   description         = "Alerta para Jobs no Databricks com alto tempo de execução"
   severity            = 2
+  enabled             = true
   frequency           = 5
   time_window         = 5
-  query               = <<QUERY
+
+  data_source_id = var.workspace_logs_id
+
+  query = <<QUERY
 AzureDiagnostics
 | where Category == "WorkspaceLogs"
 | where OperationName == "JobRun"
 | where RunDuration > 3600
 | summarize Longos = count() by bin(TimeGenerated, 5m)
+| where Longos > 0
 QUERY
 
   trigger {
@@ -63,6 +69,5 @@ QUERY
   action {
     action_group_id = azurerm_monitor_action_group.alert_email_group.id
   }
-
 }
 
