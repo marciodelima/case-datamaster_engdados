8537c55201afa5a8b77952d7ad3a0a123d3ae424
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-26 21:32:06 -0300

    Ajustes

--- iac/infra/modules/databricks_provisioning/variables.tf
+++ iac/infra/modules/databricks_provisioning/variables.tf
@@ -6,9 +6,4 @@ variable "databricks_workspace_url" {
   type = string
 }
 
-variable "bootstrap_token" {
-  type      = string
-  sensitive = true
-}
-
 variable "github_repo" {default="marciodelima/case-datamaster_engdados"}
