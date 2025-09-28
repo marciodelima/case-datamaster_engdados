6936cc6eaf45aa3fda41569562f39b6c99ba1076
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-28 20:57:07 -0300

    Ajustes

--- iac/infra/modules/fabric_workspace/variables.tf
+++ iac/infra/modules/fabric_workspace/variables.tf
@@ -3,3 +3,5 @@ variable "location" {}
 variable "resource_group_name" {}
 variable "log_analytics_workspace_id" {}
 variable "admin_email" { default = "marcio.lima.f1rst@gmail.com" }
+variable "spn_name" { default = "github-deploy-spn" }
+
--- iac/infra/security.tf
+++ iac/infra/security.tf
@@ -25,16 +25,3 @@ resource "azurerm_role_assignment" "fabric_pg_access" {
   ]
 }
 
-data "azurerm_resource_group" "fabric_rg" {
-  name = var.resource_group_name
-}
-
-data "azuread_service_principal" "fabric_spn" {
-  display_name = var.spn_name
-}
-
-resource "azurerm_role_assignment" "fabric_admin" {
-  scope                = data.azurerm_resource_group.fabric_rg.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azuread_service_principal.fabric_spn.object_id
-}
