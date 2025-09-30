9a18bbfe3d63c613e11612a039532ace8f0dd871
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-09-29 21:23:36 -0300

    Ajustes Functions

---
+++ iac/infra/modules/azure_functions/security.tf
@@ -0,0 +1,63 @@
+data "azurerm_storage_account" "existing_storage" {
+  name                = var.existing_storage_account_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_role_assignment" "news_blob_access" {
+  scope                = data.azurerm_storage_account.existing_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.news_producer.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "ri_blob_access" {
+  scope                = data.azurerm_storage_account.existing_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.ri_resumer.identity.principal_id
+}
+
+data "azurerm_key_vault" "kv" {
+  name                = var.keyvault_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_eventhub_namespace" "existing_ns" {
+  name                = var.eventhub_namespace_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_role_assignment" "ri_keyvault_reader" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_function_app.ri_resumer.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "news_keyvault_reader" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_function_app.news_producer.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "news_eventhub_sender" {
+  scope                = data.azurerm_eventhub_namespace.existing_ns.id
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = azurerm_function_app.news_producer.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "ri_collector_blob_access" {
+  scope                = data.azurerm_storage_account.existing_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.ri_collector.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "finance_blob_access" {
+  scope                = data.azurerm_storage_account.existing_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.finance_csv_ingestor.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "postgres_blob_access" {
+  scope                = data.azurerm_storage_account.existing_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app.postgres_ingestor.identity.principal_id
+}
+
---
+++ iac/infra/modules/azure_functions/storage.tf
@@ -0,0 +1,44 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "azurerm_storage_account" "news_storage" {
+  name                     = "newsfuncstorage${random_id.suffix.hex}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "ri_storage" {
+  name                     = "rifuncstorage${random_id.suffix.hex}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "ri_collector_storage" {
+  name                     = "ricollectstorage${random_id.suffix.hex}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "finance_storage" {
+  name                     = "financefuncstorage${random_id.suffix.hex}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "postgres_storage" {
+  name                     = "postgresfuncstorage${random_id.suffix.hex}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
