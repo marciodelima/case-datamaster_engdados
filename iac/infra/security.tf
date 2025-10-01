31c9c8a4977625c946ee0062ab4068b16968e34c
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-10-01 19:23:19 -0300

    Databricks

--- iac/infra/security.tf
+++ iac/infra/security.tf
@@ -18,11 +18,28 @@ resource "azurerm_role_assignment" "identity_storage_access_dbx" {
 
 resource "azurerm_role_assignment" "access_connector_keyvault_access" {
   scope                = data.azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
+  role_definition_name = "Key Vault Reader"
   principal_id         = module.databricks.access_connector_principal_id
 
   depends_on = [
     module.databricks
   ]
 }
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault_access_policy" "github_owner_policy" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.databricks.access_connector_principal_id
+
+  secret_permissions = [
+    "Get", "Set", "Delete", "Recover", "List"
+  ]
+
+  depends_on = [
+    module.databricks
+  ]
+}
+
 
