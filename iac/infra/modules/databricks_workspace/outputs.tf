1eeb4260711ed9a3b80e431f39ffb3bc7708e357
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-10-01 01:47:39 -0300

    Novos Functions

--- iac/infra/modules/databricks_workspace/outputs.tf
+++ iac/infra/modules/databricks_workspace/outputs.tf
@@ -18,6 +18,6 @@ output "access_connector_id" {
 }
 
 output "access_connector_principal_id" {
-  value = azurerm_databricks_access_connector.finance.identity.principal_id
+  value = azurerm_databricks_access_connector.finance.identity[0].principal_id
 }
 
