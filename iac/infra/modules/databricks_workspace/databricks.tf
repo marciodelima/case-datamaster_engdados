06979d0bd704c84c7f6b02c7f4c6f0b35519c571
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-10-01 21:35:19 -0300

    Databricks

--- iac/infra/modules/databricks_workspace/databricks.tf
+++ iac/infra/modules/databricks_workspace/databricks.tf
@@ -5,12 +5,23 @@ resource "azurerm_databricks_workspace" "dbx" {
   sku                 = "premium"
 }
 
-resource "azurerm_databricks_access_connector" "finance" {
-  name                = "finance-access-connector"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+resource "azurerm_databricks_access_connector" "unity_catalog" {
+  name                = "unity-catalog-access-connector"
+  resource_group_name = azurerm_databricks_workspace.dbx.resource_group_name
+  location            = azurerm_databricks_workspace.dbx.location
   identity {
     type = "SystemAssigned"
   }
+  depends_on = [azurerm_databricks_workspace.dbx]
+}
+
+resource "azurerm_databricks_access_connector" "finance" {
+  name                = "finance-access-connector"
+  resource_group_name = azurerm_databricks_workspace.dbx.resource_group_name
+  location            = azurerm_databricks_workspace.dbx.location
+  identity {
+    type = "SystemAssigned"
+  }
+  depends_on = [azurerm_databricks_workspace.dbx]
 }
 
