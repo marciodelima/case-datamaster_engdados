ca875e8facbb5cf3576bef302928f6f5f30ec38b
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-10-10 19:17:57 -0300

    Ajustes App

--- iac/infra/modules/azure_functions/security.tf
+++ iac/infra/modules/azure_functions/security.tf
@@ -6,13 +6,13 @@ data "azurerm_storage_account" "existing_storage" {
 resource "azurerm_role_assignment" "news_blob_access" {
   scope                = data.azurerm_storage_account.existing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app_flex_consumption.news_producer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.news_producer.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "ri_blob_access" {
   scope                = data.azurerm_storage_account.existing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app_flex_consumption.ri_resumer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.ri_resumer.identity[0].principal_id
 }
 
 data "azurerm_key_vault" "kv" {
@@ -28,66 +28,66 @@ data "azurerm_eventhub_namespace" "existing_ns" {
 resource "azurerm_role_assignment" "ri_keyvault_reader" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.ri_resumer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.ri_resumer.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "news_keyvault_reader" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.news_producer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.news_producer.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "news_eventhub_sender" {
   scope                = data.azurerm_eventhub_namespace.existing_ns.id
   role_definition_name = "Azure Event Hubs Data Sender"
-  principal_id         = azurerm_function_app_flex_consumption.news_producer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.news_producer.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "ri_collector_blob_access" {
   scope                = data.azurerm_storage_account.existing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app_flex_consumption.ri_collector.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.ri_collector.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "finance_blob_access" {
   scope                = data.azurerm_storage_account.existing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app_flex_consumption.finance_csv_ingestor.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.finance_csv_ingestor.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "postgres_blob_access" {
   scope                = data.azurerm_storage_account.existing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app_flex_consumption.postgres_ingestor.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.postgres_ingestor.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "sentiment_blob_access" {
   scope                = data.azurerm_storage_account.existing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app_flex_consumption.news_sentiment_analyzer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.news_sentiment_analyzer.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "sentiment_keyvault_access" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.news_sentiment_analyzer.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.news_sentiment_analyzer.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "ri_collector_keyvault_reader" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.ri_collector.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.ri_collector.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "finance_keyvault_reader" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.finance_csv_ingestor.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.finance_csv_ingestor.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "postgres_keyvault_reader" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app_flex_consumption.postgres_ingestor.identity[0].principal_id
+  principal_id         = azurerm_linux_function_app.postgres_ingestor.identity[0].principal_id
 }
 
