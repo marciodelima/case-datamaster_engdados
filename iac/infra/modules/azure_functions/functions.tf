db161bfd71ef3d63963ecfe9565f358030b18ed2
Marcio de Lima <marcio_de_lima@yahoo.com.br>
2025-10-10 17:31:03 -0300

    Ajustes App

--- iac/infra/modules/azure_functions/functions.tf
+++ iac/infra/modules/azure_functions/functions.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "func_plan" {
   name                = "func-plan"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku_name            = "FC1"
+  sku_name            = "EP1"
   os_type             = "Linux"
 }
 
